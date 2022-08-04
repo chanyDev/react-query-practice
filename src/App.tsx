dd0cdea76747bb1e8dd7efe6f864b75f8227612a
chanyDev <chanyd.dev@gmail.com>
2022-08-04 17:53:00 +0900

    feat : react-query 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
@@ -6,15 +7,19 @@ import ReactQueryUserList from './components/ReactQueryUserList';
 import UserList from './components/UserList';
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/without-react-query' element={<UserList />} />
-        <Route path='/with-react-query' element={<ReactQueryUserList />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/without-react-query' element={<UserList />} />
+          <Route path='/with-react-query' element={<ReactQueryUserList />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/components/ReactQueryUserList.tsx
+++ src/components/ReactQueryUserList.tsx
@@ -1,5 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface UserResponse {
+  id: number;
+  name: string;
+  age: number;
+}
+
 const ReactQueryUserList = () => {
-  return <h1>With React Query</h1>;
+  const getUsers = (): Promise<UserResponse[]> =>
+    axios.get('http://localhost:4000/users').then(res => res.data);
+
+  const { data, isLoading, error } = useQuery(['users'], getUsers);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error ❗❗</div>;
+
+  return (
+    <>
+      <h1>With React Query</h1>
+      <h2>UserList</h2>
+      <ul className='user-list'>
+        {data?.map(user => (
+          <li key={user.id} className='user-item'>
+            <div>name: {user.name}</div>
+            <div>age: {user.age}</div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ReactQueryUserList;
